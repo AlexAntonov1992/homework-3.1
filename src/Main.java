5350de40ee26e000ededf092e9ee68329e2d765b
user <barbizylka@gmail.com>
2025-06-19 14:44:57 +0300

    Сделано д 3.4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, !");
+        System.out.println("Hello !");
 
 //Задача 1
         int cclientOS = 0;
